10c278059714135ee2f1ee3cc895ab74a5bc07b8
IngeniousMaglev <shibeswarsingha@gmail.com>
2025-03-01 00:51:45 +0530

    Initaial Commit

---
+++ js/commands.js
@@ -0,0 +1,154 @@
+let linkedin = 'https://www.linkedin.com/in/sibeswar-singha-954a54166/';
+let github = 'https://github.com/IngeniousMaglev';
+let email = 'mailto:shibeswarsingha@gmail.com';
+let repo = 'https://github.com/IngeniousMaglev/interactive-terminal';
+let facebook = 'https://www.facebook.com/shibeswar.singha';
+let existCommands = [
+  'about',
+  'achievements',
+  'banner',
+  'certificates',
+  'clear',
+  'cls',
+  'date',
+  'education',
+  'email',
+  'github',
+  // 'gui',
+  'help',
+  'history',
+  'linkedin',
+  'projects',
+  'repo',
+  'social',
+  'whoami',
+];
+
+about = [
+  'Hello, I\'m <span style="font-weight:bold">Sibeswar Prasad Singha</span>👋.',
+  '<br>',
+  'A highly motivated and seasoned <span style="font-weight:bold">Cybersecurity Professional</span> with over three years of experience ',
+  '<br>',
+  'I am currently working as an IAM analyst at Tata Consultancy Services.'
+];
+
+
+achievements = [
+  'Here comes some of my achievements',
+  '<br>',
+  '<span>Acknowledged by <span style="font-weight:bold">NCIIPC</span> for reporting a vulnerability in the website of the <span style="font-weight:bold">Ministry of Heavy Industries</span></span>',
+  '<br>',
+  '<span>Acknowledged by <span style="font-weight:bold">NCIIPC</span> for reporting a vulnerability in the website of the <span style="font-weight:bold">Karnataka Government App\'s Website</span></span>',
+  '<br>',
+  '<span>Acknowledged by <span style="font-weight:bold">NCIIPC</span> for reporting a vulnerability in the website of the <span style="font-weight:bold">Smart Cities Mission Website</span></span>',
+  '<br>',
+  '<span>Acknowledged by <span style="font-weight:bold">NCIIPC</span> for reporting a vulnerability in the website of the <span style="font-weight:bold">Maharastra Forest Department ', 
+  'Admin Login Webpage</span></span>',
+];
+
+
+banner = [
+  '<span class="command">     .........                          </span>',
+  '<span class="command">              .::::-:                   </span>',
+  '<span class="command">    ........:::::::-+                   </span>',
+  '<span class="command">  .         .:::::--:=.    .            </span>',
+  '<span class="command">       .....         =#*+++=-...            </span>',
+  '<span class="command">     ..            :*=.    .-=+*=.         </span>',
+  '<span class="command">                   *-          .*#:        </span>',
+  '<span class="command">                   #-             =:       </span>',
+  '<span class="command">                   :*-.                     </span>',
+  '<span class="command">                     -=+====-::.             </span>',
+  '<span class="command">                            .-+#+-=-        </span>',
+  '<span class="command">                                =+..-:      </span>',
+  '<span class="command">                                  +- ..    </span>',
+  '<span class="command">                                   =:   </span>',
+  '<span class="command">                                    =   </span>',
+  '<span class="command">                                    .   </span>',
+  '<span class="command">                                    .   </span>',
+  '<span class="color2">Hello and welcome to my interactive web terminal!</span>',
+  '<span class="color2">If you would like to see a list of the commands that are available, please type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
+
+
+certificates = [
+  'Here are my certificates!',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">Web-Developement</span>',
+  '<span style="">Internshala Training</span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">Machine Learning with Python</span>',
+  '<span style="">Globsyn</span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">Ethical Hacking and Security Fundamentals</span>',
+  '<span style="">Globsyn</span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">Data Analysis with Python</span>',
+  '<span style="">Fortinet</span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">Programming with Python</span>',
+  '<span style="">MyWBUT</span>',
+];
+
+
+education = [
+  'Here is my education background!',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">B.Tech in Computer Science Engineering</span>',
+  '<span style="">Government College of Engineering and Ceramic Technology | Kolkata, India | 2017 ~ 2021</span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">High Secondary</span>',
+  '<span style="">B. E. Model School | Howrah, West Bengal | 2014 ~ 2016</span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">Secondary</span>',
+  '<span style="">B. E. Model School | Howrah, West Bengal | 2002 ~ 2014</span>',
+];
+
+
+help = [
+  '<span class="command">about</span>              Who is Ingenious Maglev?',
+  '<span class="command">banner</span>             Display the header',
+  // '<span class="command">achievements</span>       Show my achievements',
+  '<span class="command">certificates</span>       Display the certificates',
+  '<span class="command">clear | cls</span>        Clear the teminal',
+  '<span class="command">date</span>               Display the actual date',
+  '<span class="command">education</span>          my education background',
+  '<span class="command">email</span>              Send me an email',
+  '<span class="command">github</span>             Open the GitHub',
+  '<span class="command">help</span>               Check available commands',
+  '<span class="command">history</span>            View command history',
+  '<span class="command">linkedin</span>           Open the LinkedIn',
+  // '<span class="command">projects</span>           View coding projects',
+  '<span class="command">repo</span>               Open the source code',
+  '<span class="command">social</span>             Display social networks',
+  '<span class="command">whoami</span>             About current user',
+  '<br>',
+  '[TAB]              Trigger completion',
+  '[Ctrl+l]           Clear terminal',
+  '[Ctrl+p]           Go back to previous command',
+];
+
+projects = [
+  '"Actions speak louder than words, demonstrate the implementation."? I got you.',
+  'Here are some of my projects!',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">AutoRecon</span>',
+  '<span style="">A simple tool built using python which performs automated enumeration of services. It is intended as a',
+  'time-saving tool for use in CTFs. It may also be useful in real-world engagements.</span>',
+  '<span>You can check it out <a href="https://github.com/ninja-hattori/AutoRecon">here</a></span>',
+  '<br>',
+  '<span style="font-weight:bold;font-size:15px">DirEnum</span>',
+  '<span style="">A tool built using python to brute force directories and files names on web/application servers.</span>',
+  '<span>You can check it out <a href="https://github.com/ninja-hattori/DirEnum">here</a></span>',
+];
+
+social = [
+  'facebook          <a href="' +
+  facebook +
+  '" target="_blank">facebook/sibeswar</a>',
+  'linkedin          <a href="' +
+  linkedin +
+  '" target="_blank">linkedin/sibeswar</a>',
+  'github            <a href="' +
+  github +
+  '" target="_blank">github/sibeswar</a>',
+];
